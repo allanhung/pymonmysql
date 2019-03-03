7bc7ec894a2aec3408e5e58ab589a80e7f28cc7c
allan <hung.allan@gmail.com>
2019-03-04 00:05:29 +0800

    ignore NULL sql

--- pymonmysql/mon_module/proc.py
+++ pymonmysql/mon_module/proc.py
@@ -24,7 +24,7 @@ from pprint import pprint
 
 def list(args):
     myobj = common.MyMon(host=args['--host'],user=args['--user'],password=args['--password'])
-    query = "select * from information_schema.PROCESSLIST where command <> 'Sleep' and user not in ('event_scheduler', 'root', 'system user')"
+    query = "select * from information_schema.PROCESSLIST where command <> 'Sleep' and user not in ('event_scheduler', 'root', 'system user') and info <> NULL"
     if args['--querytime']:
         query += " and time > {}".format(args['--querytime'])
     query = "{} order by time desc limit {}".format(query, args['--rc']) if args['--rc'] else "{} order by time desc".format(query)
@@ -34,7 +34,7 @@ def list(args):
 
 def export(args):
     myobj = common.MyMon(host=args['--host'],user=args['--user'],password=args['--password'])
-    query = "select * from information_schema.PROCESSLIST where command <> 'Sleep' and user not in ('event_scheduler', 'root', 'system user')"
+    query = "select * from information_schema.PROCESSLIST where command <> 'Sleep' and user not in ('event_scheduler', 'root', 'system user') and info <> NULL"
     if args['--querytime']:
         query += " and time > {}".format(args['--querytime'])
     output_filename = '/tmp/mysql_proc_{}.txt'.format(time.strftime("%Y%m%d%H%M%S", time.localtime()))
